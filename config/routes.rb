e36229970bbeede90079cc8dcbdf669432354eaf
josh125 <jpdelossantos25@gmail.com>
2016-07-26 13:05:24 +0000

    added user and validation

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
 
-  root 'articles#index'
+  root 'pages#home'
   get 'about', to: 'pages#about'
 
   resources :articles
